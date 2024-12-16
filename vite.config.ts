c4860facbf3cf0cf990060d410ca93f9b01dc323
Aya <90342033+Aya-X@users.noreply.github.com>
2024-12-16 16:29:04 +0800

    perf: Update to enhance Vite configuration with build optimizations and asset management

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,29 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    cssCodeSplit: true,
+    sourcemap: false,
+    // minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        pure_funcs: ['console.log'],
+      },
+      format: {
+        comments: false,
+      },
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-react': ['react', 'react-dom'],
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
+      },
+    },
+  },
+  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg'],
 })
